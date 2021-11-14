9a828ee351f29bf64e7b297a371bd17b6d84c337
Lwrless <happy.lwr@gmail.com>
2021-11-14 20:01:04 +0800

    * add basic features.

--- src/extension.js
+++ src/extension.js
@@ -1,25 +1,95 @@
 const vscode = require('vscode');
+const {default: axios} = require('axios');
 
-let baseURL = '';
+let baseURL, token = '';
+
+const isTerminalExist = () => vscode.window.terminals.length !== 0;
+const executeCommandInTerminal = command => {
+    const terminal = isTerminalExist() ? vscode.window.activeTerminal : vscode.window.createTerminal();
+	terminal.show();
+	terminal.sendText(command, false); // change to true for instant execution.
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
-	context.subscriptions.push(vscode.commands.registerCommand('zentao.getURL', function () {
-		vscode.window.showInformationMessage(baseURL);
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('zentao.setURL', async url => {
-		if (!url) {
-			url = await vscode.window.showInputBox({
-				placeHolder: "https://demo.zentao.com",
-				prompt: "Enter zentao base url"
+const activate = (context) => {
+	context.subscriptions.push(vscode.commands.registerCommand('zentao.login', async () => {
+		let account, password;
+		const url = await vscode.window.showInputBox({
+			placeHolder: 'http://172.31.44.116/zentao/',
+			value: 'http://172.31.44.116/zentao/',
+			validateInput: input => /https?:\/{2}.*\//.test(input) ? '' : '请输入完整的禅道 URL',
+			prompt: '输入禅道后端地址',
+			ignoreFocusOut: true,
+		});
+		if (url) {
+			baseURL = `${url}`;
+			account = await vscode.window.showInputBox({
+				placeHolder: 'admin',
+				value: 'admin',
+				prompt: '输入禅道用户名',
+				validateInput: input => input.length ? '' : '请输入用户名',
+				ignoreFocusOut: true,
 			});
 		}
-		if (url) {
-			baseURL = url;
+		if (account) {
+			password = await vscode.window.showInputBox({
+				placeHolder: '******',
+				value: '123456',
+				prompt: '输入禅道密码',
+				validateInput: input => input.length ? '' : '请输入密码',
+				password: true,
+				ignoreFocusOut: true,
+			});
 		}
+
+		if (!url || !account || !password) {
+			return vscode.window.showWarningMessage('信息不完整，登录终止');
+		}
+		vscode.window.showInformationMessage(`将使用 ${account}:${'*'.repeat(password.length)} 在 ${url} 登录`);
+		axios.post(`${baseURL}api.php/v1/tokens`, {account, password}, {
+			headers: {'Content-Type': 'application/json'}
+		}).then(res => {
+			({token} = res.data);
+			vscode.window.showInformationMessage(`登录成功，获得 token: ${token}`);
+		});
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('zentao.pickTasksForCommit', () => {
+		if (!token) {
+			return vscode.window.showErrorMessage('请先登录禅道');
+		}
+		axios.get(`${baseURL}my-work-task.json`, {
+			headers: {'Content-Type': 'application/json', 'Token': token}
+		}).then(res => {
+			const resData = JSON.parse(res.data.data);
+			vscode.window.showQuickPick(resData.tasks.map(task => `任务 #${task.id}: ${task.name}`), {
+				canPickMany: true,
+			}).then(items => {
+				const taskText = items.reduce((pv, cv) => pv += `${/ (#\d+): /.exec(cv)[1]}, `, 'task ').replace(/, $/, '');
+				vscode.window.showInputBox({
+					prompt: '输入 Commit Message 内容',
+					ignoreFocusOut: true,
+				}).then(text => {
+					executeCommandInTerminal(`git commit -m "* ${text.replace('"', '\\"')}, ${taskText}."`);
+				})
+			});
+		});
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('zentao.viewTasks', () => {
+		if (!token) {
+			return vscode.window.showErrorMessage('请先登录禅道');
+		}
+		axios.get(`${baseURL}my-work-task.json`, {
+			headers: {'Content-Type': 'application/json', 'Token': token}
+		}).then(res => {
+			const resData = JSON.parse(res.data.data);
+			vscode.window.showQuickPick(resData.tasks.map(task => `任务 #${task.id}: ${task.name}`)).then(item => {
+				vscode.env.openExternal(`${baseURL}task-view-${/ #(\d+): /.exec(item)[1]}.html`);
+			});
+		});
 	}));
 }
 
