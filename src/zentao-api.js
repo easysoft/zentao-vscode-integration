cd52c23ca989da433db72b73bc3cc96d7d3f9c33
Lwrless <happy.lwr@gmail.com>
2021-12-09 13:56:10 +0800

    * add apis and refactor.

--- src/zentao-api.js
+++ src/zentao-api.js
@@ -5,16 +5,19 @@ const {default: axios} = require('axios');
 class zentaoAPI {
     constructor(context) {
         /**
+         * 扩展上下文（扩展激活时创建的）
          * @type {vscode.ExtensionContext}
          */
         this._context = context;
 
         /**
+         * API Token
          * @type {string}
          */
         this._token = '';
 
         /**
+         * API URL
          * @type {string}
          */
         this._baseURL = '';
@@ -49,13 +52,33 @@ class zentaoAPI {
     }
 
     /**
+     * GET 一个禅道 API
+     * @param {string} path 路径
+     * @returns {Promise<import('axios').AxiosResponse>}
+     */
+    get(path) {
+        return axios.get(`${this._baseURL}api.php/v1/${path}`, {
+            headers: {'Content-Type': 'application/json', Token: this._token}
+        });
+    }
+
+    /**
+     * POST 一个禅道 API
+     * @param {string} path 路径
+     * @returns {Promise<import('axios').AxiosResponse>}
+     */
+    post(path, data) {
+        return axios.post(`${this._baseURL}api.php/v1/${path}`, data, {
+            headers: {'Content-Type': 'application/json', Token: this._token}
+        });
+    }
+
+    /**
      * 获取当前用户
      * @returns {object} 当前登录的用户
      */
     async getCurrentUser() {
-        const response = await axios.get(`${this._baseURL}api.php/v1/user`, {
-            headers: {'Content-Type': 'application/json', Token: this._token}
-        });
+        const response = await this.get('user');
         return response.data.profile;
     }
 
@@ -65,6 +88,7 @@ class zentaoAPI {
      * @param {string} [credentials.url] 禅道地址
      * @param {string} [credentials.account] 用户名
      * @param {string} [credentials.password] 密码
+     * @returns {object} 当前登录的用户信息
      */
     async login(credentials) {
         if (credentials) {
@@ -84,6 +108,64 @@ class zentaoAPI {
             await this.setCredentials({token});
         }
         return await this.getCurrentUser(credentials);
+    }
+
+    /**
+     * 获取产品列表
+     * @returns {object[]} 产品列表
+     */
+    async getProducts() {
+        const response = await this.get('products');
+        return response.data.products;
+    }
+
+    /**
+     * 获取产品详情
+     * @param {number} product 产品 ID
+     * @returns {object} 产品
+     */
+    async getProduct(product) {
+        const response = await this.get(`products/${product}`);
+        return response.data;
+    }
+
+    /**
+     * 获取产品的需求
+     * @param {number} product 产品 ID
+     * @returns {object[]} 需求列表
+     */
+    async getProductStories(product) {
+        const response = await this.get(`products/${product}/stories`);
+        return response.data.stories;
+    }
+
+    /**
+     * 获取产品的 bugs
+     * @param {number} product 产品 ID
+     * @returns {object[]} bug 列表
+     */
+    async getProductBugs(product) {
+        const response = await this.get(`products/${product}/bugs`);
+        return response.data.bugs;
+    }
+
+    /**
+     * 获取执行的任务列表
+     * @param {number} execution 执行 ID
+     * @returns {object[]} 任务列表
+     */
+    async getExecutionTasks(execution) {
+        const response = await this.get(`executions/${execution}/tasks`);
+        return response.data.tasks;
+    }
+
+    /**
+     * 获取版本库列表
+     * @returns {object[]} 版本库列表
+     */
+    async getRepos() {
+        const response = await this.get('repos');
+        return response.data.repos;
     }
 }
 
