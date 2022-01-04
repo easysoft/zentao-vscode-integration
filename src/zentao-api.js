a6a3c6dfa19ca15c26c14ed2bd8bbd9fd71e0178
Lwrless <happy.lwr@gmail.com>
2022-01-04 15:21:14 +0800

    * fix getAll always returning first response.

--- src/zentao-api.js
+++ src/zentao-api.js
@@ -156,7 +156,7 @@ class zentaoAPI {
         if (!firstResponse) {
             return;
         }
-        if (!firstResponse.page || firstResponse.data.limit >= firstResponse.data.total) {
+        if (!firstResponse.data.page || firstResponse.data.limit >= firstResponse.data.total) {
             return firstResponse;
         }
         return await this.get(`${path}?limit=${firstResponse.data.total}`);
